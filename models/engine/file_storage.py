918df796e8ee0eff15690dbae0f9f94cff06fa3c
Jp203 <jp203@icloud.com>
2019-11-13 16:37:50 -0500

    added reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """serializes instances to JSON file and deserializes JSON file to instances"""
-
-
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """ Class for FileStorage """
@@ -20,5 +26,21 @@ class FileStorage:
 
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
+    def save(self):
+        """ serializes __objects to Json file """
+        a_dict = {}
+        for key in FileStorage.__objects:
+            a_dict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, mode="w",
+                encoding="utf-8") as a_file:
+            json.dump(a_dict, a_file)
+
     def reload(self):
         """ Desirialization json """
+        try:
+            with open(FileStorage.__file_path, mode="r",
+                    encoding="utf-8") as a_file:
+                a_dict = json.load(a_file)
+            for key, value in a_dict.items():
+                FileStorage.__objects[key] = eval(value['__class__'])(**value)
+        except FileNotFoundError: pass
